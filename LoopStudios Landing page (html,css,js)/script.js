383760bbc7873c063975ad0d07ba222c5ba5bd61
PaulChristian2021 <85711813+PaulChristian2021@users.noreply.github.com>
2022-01-05 02:51:02 +0800

    Code format changes

--- LoopStudios Landing page (html,css,js)/script.js
+++ LoopStudios Landing page (html,css,js)/script.js
@@ -1,1 +1,22 @@
-const nav=document.querySelector(".nav"),menu=document.querySelector("#menu");menu.addEventListener("click",()=>{menuToggle()}),window.addEventListener("resize",()=>{800<=window.innerWidth?nav.style.display="flex":mobileNav()});function menuToggle(){menu.classList.contains("hamburger")?(menu.classList.add("close"),menu.classList.remove("hamburger"),nav.style.display="flex",document.body.style.position="fixed"):mobileNav()}function mobileNav(){menu.classList.add("hamburger"),menu.classList.remove("close"),nav.style.display="none",document.body.style.position=""}
+const nav = document.querySelector(".nav"),
+  menu = document.querySelector("#menu");
+menu.addEventListener("click", () => {
+  menuToggle();
+}),
+  window.addEventListener("resize", () => {
+    800 <= window.innerWidth ? (nav.style.display = "flex") : mobileNav();
+  });
+function menuToggle() {
+  menu.classList.contains("hamburger")
+    ? (menu.classList.add("close"),
+      menu.classList.remove("hamburger"),
+      (nav.style.display = "flex"),
+      (document.body.style.position = "fixed"))
+    : mobileNav();
+}
+function mobileNav() {
+  menu.classList.add("hamburger"),
+    menu.classList.remove("close"),
+    (nav.style.display = "none"),
+    (document.body.style.position = "");
+}
